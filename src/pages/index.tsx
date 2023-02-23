2bf770b03411ff068f7e666e330fe55d3c553a36
James Zhang <jameszhanga@gmail.com>
2023-02-23 01:13:08 -0500

    add sorting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const rizzAPI = api.rizz;
 const Home: NextPage = () => {
   const [cook, cooking] = useState("");
   const [rizz, rizzling] = useState<Rizz>([]);
+  const [order, setOrder] = useState<string>("vote");
 
   const { getAll, submit, upvote, downvote } = rizzAPI;
 
@@ -56,6 +57,21 @@ const Home: NextPage = () => {
     cooking("");
   };
 
+  const handleOrder = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setOrder(e.target.value);
+    switch (e.target.value) {
+      case "vote":
+        rizzling((prev) => [...prev].sort((a, b) => b.votes - a.votes));
+        break;
+
+      case "alphabetical":
+        rizzling((prev) =>
+          [...prev].sort((a, b) => a.rizz.localeCompare(b.rizz))
+        );
+        break;
+    }
+  };
+
   return (
     <>
       <Head>
@@ -64,14 +80,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <div className="px-4 py-16">
+          <h1 className="text-xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             The <span className="text-[hsl(280,100%,70%)]">Rizzclopedia</span>
           </h1>
         </div>
 
         <div>
-          <form onSubmit={handleSubmit}>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-row items-center justify-center"
+          >
             <input
               type="text"
               name="habit"
@@ -79,23 +98,44 @@ const Home: NextPage = () => {
               value={cook}
               onChange={(e) => cooking(e.target.value)}
             />
-            <button type="submit">create</button>
+            <button
+              type="submit"
+              className="mx-1 rounded bg-sky-400 px-3 hover:bg-sky-300"
+            >
+              add
+            </button>
           </form>
 
           <ul className="flex flex-col">
+            <label htmlFor="order" className="text-white">
+              Sort:
+            </label>
+
+            <select
+              name="order"
+              id="order"
+              value={order}
+              onChange={handleOrder}
+              className="w-24 rounded"
+            >
+              <option value="vote">by vote</option>
+              <option value="alphabetical">by name</option>
+            </select>
+            <div className="m-7" />
+
             {rizz.map((rizz) => (
               <li key={rizz.id}>
-                <div className="around flex w-[70vw] flex-row justify-between rounded-lg text-white">
+                <div className="around flex w-[70vw] flex-row justify-between rounded-lg pb-2 text-white">
                   <span className="capitalize">{rizz.rizz}</span>
 
-                  <div>
+                  <div className="flex flex-row">
                     <button
                       onClick={() => upvoteCreation.mutate({ id: rizz.id })}
                       className="mx-2 rounded bg-sky-400 px-3 hover:bg-sky-300"
                     >
                       +
                     </button>
-                    {rizz.votes}
+                    <div className="min-w-[2rem] text-center">{rizz.votes}</div>
                     <button
                       onClick={() => downvoteCreation.mutate({ id: rizz.id })}
                       className="mx-2 rounded bg-sky-400 px-3 hover:bg-sky-300"
